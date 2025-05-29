610fd58dcf0675054ec302cb59db94a4514b969e
csnanx <210231094+csnanx@users.noreply.github.com>
2025-05-29 22:55:37 +0530

    Refactor to remove unnecessary nesting

--- main.py
+++ main.py
@@ -6,15 +6,16 @@ MENU = Menu()
 coffee_maker = CoffeeMaker()
 money_machine = MoneyMachine()
 
-user_choice = ""
-while user_choice != "off":
+off = False
+while not off:
     user_choice = input(f"What would you like? {MENU.get_items()}: ")
     if user_choice == "report":
         coffee_maker.report()
         money_machine.report()
-    if user_choice != "off" and user_choice != "report":
+    elif user_choice == "off":
+        off = True
+    else:
         ordered_item = MENU.find_drink(user_choice)
-        
         if coffee_maker.is_resource_sufficient(ordered_item):
             if money_machine.make_payment(ordered_item.cost):
                 coffee_maker.make_coffee(ordered_item)
